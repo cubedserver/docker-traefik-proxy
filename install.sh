2a6195df9def23dee0fed6e938c17da0cf327c3c
Fábio Assunção <fabio23gt@gmail.com>
2021-10-03 16:46:00 -0300

    Name changes, configuration adjustments and small improvements

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-DOCKER_NETWORKS="web internal"
-DOCKER_COMPOSE_FILE="docker-compose.yml"
+: ${DOCKER_NETWORKS:='web internal'}
+: ${DOCKER_COMPOSE_FILE:='docker-compose.yml'}
+: ${ADDITIONAL_APPS:='adminer mysql phpmyadmin portainer postgres redis whoami wordpress'}
 
 function setup_log() {
-  echo -e "\033[1;32m$*\033[m"
+  echo -e $1
 }
 
 for NETWORK_NAME in $DOCKER_NETWORKS; do
     setup_log "⚡ Creating Docker network ${NETWORK_NAME}"
     docker network ls|grep $NETWORK_NAME > /dev/null || docker network create $NETWORK_NAME
-done 
-
+done
 
 if [[ ! -e acme.json ]]; then
 	touch acme.json
@@ -19,3 +19,15 @@ if [[ ! -e acme.json ]]; then
 fi
 
 docker-compose -f $DOCKER_COMPOSE_FILE up -d
+
+if [[ ! -z $ADDITIONAL_APPS ]]; then
+    for APP in $ADDITIONAL_APPS; do
+        if [ -d templates/${APP} ]; then
+
+            setup_log "---> ⚡ Starting ${APP} container"
+            docker-compose -f templates/${APP}/docker-compose.yml up -d
+        else
+            setup_log "---> ❌ App ${APP} files not found. Skipping..."
+        fi
+    done    
+fi
